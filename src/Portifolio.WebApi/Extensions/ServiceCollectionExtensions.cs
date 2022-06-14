c81b274184686914f002bdf49c4c122e8a9c97c8
pedro furlan <pedro.furlan@globalsys.com.br>
2022-06-14 16:21:31 -0300

    Renomeação WebApiPortifolio para Portifolio.WebApi

---
+++ src/Portifolio.WebApi/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using AutoMapper;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Portifolio.Domain.Command.Commands.Request.Works.Create;
+using Portifolio.Domain.Command.Handlers.Works.Create;
+using Portifolio.Domain.Command.Profiles.Works;
+using Portifolio.Domain.Generics;
+using Portifolio.Domain.ITextSharp;
+using Portifolio.Domain.MinIO;
+using Portifolio.Domain.Query.Configurations;
+using Portifolio.Infrastructure.Database.EntityFramework.Generics;
+using Portifolio.Utils.ITextSharpResumeUtils;
+using Portifolio.Utils.MinIO;
+using Portifolio.WebApi.Controllers;
+
+namespace Portifolio.WebApi.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMediator(this IServiceCollection services)
+        {
+            services.AddMediatR(typeof(CreateWorksRequest), typeof(WorksController));
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureAutoMapper(this IServiceCollection services)
+        {
+            var profiles = typeof(WorksProfile).Assembly.GetTypes().Where(x => typeof(Profile).IsAssignableFrom(x));
+
+            var mapperConfig = new MapperConfiguration(r =>
+            {
+                profiles.ToList().ForEach(p => r.AddProfile(p));
+            });
+
+            IMapper mapper = mapperConfig.CreateMapper();
+
+            services.AddSingleton(mapper);
+
+            return services;
+        }
+
+        public static IServiceCollection AddGeneralServices(this IServiceCollection services)
+        {
+            services
+                .AddTransient<ITextSharpUtils, ServicePDFResume>()
+                .AddTransient<IMinIO, MinIOUtils>();
+
+            return services;
+        }
+   
+        public static IServiceCollection AddQueryServices(this IServiceCollection services)
+        {
+            services
+                 .AddTransient(typeof(IGenericQuery<,>), typeof(DapperDefaultSearch<,>));
+                
+            return services;
+        }
+
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
+            services
+                .AddTransient(typeof(IGenericRepository<>), typeof(RepositoryGenerics<>));
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureFluentValidation(this IServiceCollection services)
+        {
+            services.AddFluentValidation(r =>
+            {
+                r.RegisterValidatorsFromAssembly(typeof(CreateWorkValidator).Assembly);
+            });
+
+            return services;
+        }
+    }
+}
---
+++ src/Portifolio.WebApi/Startup.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using Portifolio.WebApi.Extensions;
+using System.IO;
+using System.Reflection;
+
+namespace Portifolio.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var location = Assembly.GetAssembly(typeof(Startup)).Location;
+            var pathXml = Path.Combine(location.Substring(0,location.IndexOf("bin")), string.Format("{0}.xml", typeof(Startup).GetTypeInfo().Assembly.GetName().Name));
+            services.AddControllers();
+            services.AddMediator();
+            services.ConfigureAutoMapper();
+            services.AddQueryServices();
+            services.AddInfrastructureServices();
+            services.AddGeneralServices();
+            services.ConfigureFluentValidation();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Portifolio.WebApi", Version = "v1" });
+                c.IncludeXmlComments(pathXml);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Portifolio.WebApi v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
