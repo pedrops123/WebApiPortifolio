acf23b5927176e099b36abf7f9e5e2800a4622f4
pedro furlan <pedro.furlan@globalsys.com.br>
2022-08-24 16:21:23 -0300

    Comeco inclusao testes unitarios works

---
+++ src/Portifolio.Domain.Test/Commands/Work/Create/CreateWorkCommandHandlerTest.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture;
+using AutoMapper;
+using MediatR;
+using Moq;
+using Portifolio.Domain.Command.Commands.Request.Works.Create;
+using Portifolio.Domain.Command.Handlers.Work.Create;
+using Portifolio.Domain.Command.Profiles.Work;
+using Portifolio.Domain.Entities;
+using Portifolio.Domain.Generics;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Portifolio.Domain.Test.Commands.Work.Create
+{
+    public sealed class CreateWorkCommandHandlerTest
+    {
+        private readonly IMapper _mapper;
+
+        public CreateWorkCommandHandlerTest()
+        {
+            _mapper = new MapperConfiguration(opt => opt.AddProfile<WorksProfile>()).CreateMapper();
+        }
+
+        [Fact]
+        public async Task Handle_ValidData_ReturnsUnitValue()
+        {
+            var fixture = new Fixture();
+            var repository = new Mock<IGenericRepository<Works>>();
+            var request = fixture.Create<CreateWorksRequest>();
+
+            var handler = new CreateWorkHandle(
+                repository.Object,
+                _mapper);
+
+            var response = await handler.Handle(request, default);
+
+            Assert.Equal(Unit.Value, response);
+
+
+            repository.Verify(r=>r.Add(It.IsAny<Works>()),Times.Once);
+        }
+    }
+}
---
+++ src/Portifolio.Domain.Test/Mocks/Works/GenericQueryRepositoryMock.cs
@@ -0,0 +1,23 @@
+﻿using Moq;
+using Portifolio.Domain.Command.Commands.Request.Works.GetList;
+using Portifolio.Domain.Generics;
+
+namespace Portifolio.Domain.Test.Mocks.Works
+{
+    public sealed class GenericQueryRepositoryMock
+    {
+        private readonly Mock<IGenericQuery<Entities.Works, FilterWorksRequest>> Instance;
+        public GenericQueryRepositoryMock()
+        {
+            Instance = new Mock<IGenericQuery<Entities.Works, FilterWorksRequest>>();
+        }
+
+        public GenericQueryRepositoryMock SetupSuccessGetList()
+        {
+            Instance.Setup(r=>r.GetList())
+
+
+            return this;
+        }
+    }
+}
